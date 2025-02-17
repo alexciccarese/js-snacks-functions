223205b4c9abef8bca946183e946a566022bacaa
alexciccarese <alex.ciccarese@gmail.com>
2025-02-17 16:20:07 +0100

    add 04-conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function returnVocals(str) {
+  const vocals = ['a', 'e', 'i', 'o', 'u']
+  const letter = []
+  
+  for (let i = 0; i < str.length; i++) {
+    if (vocals.includes(str[i].toLowerCase())) {
+      letter.push(str[i])
+    }
+    
+  }
+  return letter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(returnVocals(word));
 
 
 
